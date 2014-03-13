ee549d9719c84065b003b5569589607f27dd0f61
magalhas <magalhas@gmail.com>
2014-03-13 00:57:50 +0000

    2 whitespaces indentation

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,32 +1,28 @@
 'use strict';
 
-module.exports = function(grunt) {
-    grunt.initConfig({
-        clean: {
-            test: ['.tmp']
+module.exports = function (grunt) {
+  grunt.initConfig({
+    clean: {
+      test: ['.tmp']
+    },
+    jscrambler: {
+      test: {
+        options: {
+          keys: grunt.file.readJSON('jscrambler_keys.json')
         },
-        jscrambler: {
-            test: {
-                options: {
-                    keys: grunt.file.readJSON('jscrambler_keys.json'),
-                    mode: 'starter'
-                },
-                files: [
-                    { src: ['Gruntfile.js'], dest: '.tmp/'},
-                    {
-                        expand: true,
-                        src: ['tasks/*.js'],
-                        dest: '.tmp/',
-                        ext: '.min.js'
-                    }
-                ]
-            }
-        }
-    });
-
-    grunt.loadTasks('tasks');
-
-    grunt.loadNpmTasks('grunt-contrib-clean');
-
-    grunt.registerTask('default', ['jscrambler']);
+        files: [
+          { src: ['Gruntfile.js'], dest: '.tmp/'},
+          {
+            expand: true,
+            src: ['tasks/*.js'],
+            dest: '.tmp/',
+            ext: '.min.js'
+          }
+        ]
+      }
+    }
+  });
+  grunt.loadTasks('tasks');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.registerTask('default', ['clean', 'jscrambler']);
 };
